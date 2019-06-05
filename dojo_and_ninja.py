97ca2fba725fc5edb1338112cf7ef4cbd8c773dd
unknown <veryweiya@gmail.com>
2019-06-05 16:15:36 -0700

    dojo and ninja practice

--- dojo_and_ninja.py
+++ dojo_and_ninja.py
@@ -15,28 +15,30 @@ Dojo.objects.create(name="B", city="San Jose", state="CA")
 Dojo.objects.create(name="C", city="Oakland", state="CA")
 
 # create 3 ninjas that belongs to the first dojo
-Ninja.objects.create(dojo=Dojo.objects.get(name="A"), first_name="Aloha", last_name="Hawaii")
-Ninja.objects.create(dojo=Dojo.objects.get(name="A"), first_name="Ola", last_name="Wakiki")
-Ninja.objects.create(dojo=Dojo.objects.get(name="A"), first_name="Chao", last_name="Tiki")
+Ninja.objects.create(dojo=Dojo.objects.first(), first_name="Aloha", last_name="Hawaii")
+Ninja.objects.create(dojo=Dojo.objects.first(), first_name="Ola", last_name="Wakiki")
+Ninja.objects.create(dojo=Dojo.objects.first(), first_name="Chao", last_name="Tiki")
 
 # create 3 ninjas that belongs to the second dojo
-Ninja.objects.create(dojo=Dojo.objects.get(name="B"), first_name="Spider", last_name="Man")
-Ninja.objects.create(dojo=Dojo.objects.get(name="B"), first_name="Pine", last_name="Apple")
-Ninja.objects.create(dojo=Dojo.objects.get(name="B"), first_name="Sharp", last_name="Fish")
+Ninja.objects.create(dojo=Dojo.objects.all()[1], first_name="Spider", last_name="Man")
+Ninja.objects.create(dojo=Dojo.objects.all()[1], first_name="Pine", last_name="Apple")
+Ninja.objects.create(dojo=Dojo.objects.all()[1], first_name="Sharp", last_name="Fish")
 
 # create 3 ninjas that belongs to the third dojo
-Ninja.objects.create(dojo=Dojo.objects.get(name="C"), first_name="Dark", last_name="Knight")
-Ninja.objects.create(dojo=Dojo.objects.get(name="C"), first_name="Stephen", last_name="King")
-Ninja.objects.create(dojo=Dojo.objects.get(name="C"), first_name="Diamond", last_name="Peak")
+Ninja.objects.create(dojo=Dojo.objects.all()[2], first_name="Dark", last_name="Knight")
+Ninja.objects.create(dojo=Dojo.objects.all()[2], first_name="Stephen", last_name="King")
+Ninja.objects.create(dojo=Dojo.objects.all()[2], first_name="Diamond", last_name="Peak")
 
 # retrieve all the ninjas from the first dojo
 Ninja.objects.filter(dojo=Dojo.objects.first())
+Dojo.objects.first().ninjas.all()
 
 # retrieve all the ninjas from the last dojo
 Ninja.objects.filter(dojo=Dojo.objects.last())
+Dojo.objects.last().ninjas.all()
 
 # retrieve the last ninja's dojo
-Ninja.objects.last()
+Ninja.objects.last().dojo
 
 # Add a new text field called "desc" to Dojo class, default value is "old dojo
 
